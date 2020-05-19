8158a113d15d76b706c837811aafef0198988dff
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-19 06:17:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,9 +45,9 @@ class ApplicationController < Sinatra::Base
   end
 
    delete '/articles/:id' do
-     @article=Article.find(params[:id])
-     @article.destroy
-     redirect to "/articles"
+    @article=Article.find(params[:id])
+    @article.destroy
+    redirect to "/articles"
    end
 
 end
