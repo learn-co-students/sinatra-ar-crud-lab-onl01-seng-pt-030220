eb403b8df273bc5663f9ab1d8233d53563d6bc50
github username <github email address>
2020-06-07 22:21:55 -0600

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    # attr_reader :article
+    # @@all=[]
+
+    # def initialize(params)
+    #     @article = params[:article]
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+
 end
